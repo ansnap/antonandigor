d91ca657db1d9af08b3c9cde3c78563993d6b685
Anton <Anton@Anton-PC>
2014-02-25 18:07:44 +0400

    Some fixes

---
+++ js/custom.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    // Header
+    $('#header a, #presentation a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 100
+        }, 500);
+        return false;
+    });
+
+    $(document).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $('#header').addClass('fixed');
+        } else {
+            $('#header').removeClass('fixed');
+        }
+    });
+
+    // Portfolio
+    $('#portfolio .work-thumbs li').click(function() {
+        $('#work-gallery').modal('toggle');
+        $('#portfolio-gallery').carousel($(this).index());
+    });
+
+    // Rates
+    function recalculate() {
+        var sum = 0;
+
+        $('#rates-expanded input:checked').each(function() {
+            sum += parseInt($(this).data('price'));
+        });
+
+        $('#rates-expanded .total-cost .sum').text(sum);
+    }
+
+    $('#rates-expanded input').change(function() {
+        recalculate();
+    });
+
+    $('#rates-expanded').collapse({
+        toggle: false
+    });
+    $('#rates .details').click(function() {
+        $('#rates-expanded').collapse('show');
+
+        $('#rates-expanded input').prop('checked', false);
+        var options = $(this).data('incl').split(',');
+        for (var i in options) {
+            var o = options[i];
+            if (o.indexOf('|') === -1) {
+                $('#rates-expanded input[name="' + o + '"]').prop('checked', true);
+            } else {
+                $('#rates-expanded input[name="' + o.split('|')[0] + '"][value="' + o.split('|')[1] + '"]').prop('checked', true);
+            }
+        }
+        recalculate();
+    });
+
+    $('#rates .order, #rates-expanded .order').click(function() {
+        $('#rates #order-window').modal();
+    });
+
+    // Questions
+    $('#questions').collapse({
+        toggle: false
+    });
+    $('#questions .title').click(function() {
+        $(this).siblings('.container').collapse('toggle');
+    });
+
+});
